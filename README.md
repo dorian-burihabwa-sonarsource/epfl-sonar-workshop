1ec0964b269995a5c285978472f9763d91c07362
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2023-09-20 11:25:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# EPFL Sonar Workshop (October 2023)
+
+This repository serves as the base project to implement custom rules for Java.
+It is derived from the [base template of the sonar-java project](https://github.com/SonarSource/sonar-java/tree/1947bdb5bec965afcee43087febf32245cb06253/docs/java-custom-rules-example).
+
+For more details about how to write custom rules, please refer to the official tutorial, [Writing Custom Java Rules 101](https://github.com/SonarSource/sonar-java/blob/1947bdb5bec965afcee43087febf32245cb06253/docs/CUSTOM_RULES_101.md).
+
+## Building the project
+
+TODO
+
+## Running
+
+TODO
+
+## License
+
+All contents under this directory are licensed under the more permissive [MIT No Attribution](LICENSE.txt) license.
---
+++ src/main/java/org/sonar/samples/java/RulesList.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2012-2023 SonarSource SA - mailto:info AT sonarsource DOT com
+ * This code is released under [MIT No Attribution](https://opensource.org/licenses/MIT-0) license.
+ */
+package org.sonar.samples.java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.sonar.plugins.java.api.JavaCheck;
+import org.sonar.samples.java.checks.AvoidAnnotationRule;
+import org.sonar.samples.java.checks.AvoidBrandInMethodNamesRule;
+import org.sonar.samples.java.checks.AvoidMethodDeclarationRule;
+import org.sonar.samples.java.checks.AvoidSuperClassRule;
+import org.sonar.samples.java.checks.AvoidTreeListRule;
+import org.sonar.samples.java.checks.MyCustomSubscriptionRule;
+import org.sonar.samples.java.checks.NoIfStatementInTestsRule;
+import org.sonar.samples.java.checks.SecurityAnnotationMandatoryRule;
+import org.sonar.samples.java.checks.SpringControllerRequestMappingEntityRule;
+
+public final class RulesList {
+
+  private RulesList() {
+  }
+
+  public static List<Class<? extends JavaCheck>> getChecks() {
+    List<Class<? extends JavaCheck>> checks = new ArrayList<>();
+    checks.addAll(getJavaChecks());
+    checks.addAll(getJavaTestChecks());
+    return Collections.unmodifiableList(checks);
+  }
+
+  /**
+   * These rules are going to target MAIN code only
+   */
+  public static List<Class<? extends JavaCheck>> getJavaChecks() {
+    return Collections.unmodifiableList(Arrays.asList(
+      SpringControllerRequestMappingEntityRule.class,
+      AvoidAnnotationRule.class,
+      AvoidBrandInMethodNamesRule.class,
+      AvoidMethodDeclarationRule.class,
+      AvoidSuperClassRule.class,
+      AvoidTreeListRule.class,
+      MyCustomSubscriptionRule.class,
+      SecurityAnnotationMandatoryRule.class));
+  }
+
+  /**
+   * These rules are going to target TEST code only
+   */
+  public static List<Class<? extends JavaCheck>> getJavaTestChecks() {
+    return Collections.unmodifiableList(Arrays.asList(
+      NoIfStatementInTestsRule.class));
+  }
+}
